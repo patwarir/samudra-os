f58445b68b1d63167f995a2522e453eafc0cd602
Rajat Patwari <rajatpatwari@gmail.com>
2023-06-26 22:41:54 -0400

    Change C arch

--- scripts/qemu_run.sh
+++ scripts/qemu_run.sh
@@ -6,4 +6,4 @@ QEMU="qemu-system-riscv64"
 # 4 Cores/HARTs
 # 512M RAM
 # Redirect IO to QEMU StdIO
-$QEMU -machine virt -cpu rv64 -smp 4 -m 512M -nographic -serial mon:stdio -bios none -kernel $1 -d guest_errors,unimp
+$QEMU -machine virt -cpu rv64,zba=true,zbb=true,zbc=true,zbs=true -smp 4 -m 512M -nographic -serial mon:stdio -bios none -kernel $1 -d guest_errors,unimp
