4aa814b592dcb8df5c28e9690d25c410e2b963ed
Rajat Patwari <rajatpatwari@gmail.com>
2023-08-07 10:25:35 -0400

    Minor dependency and memory updates

--- scripts/spike.sh
+++ scripts/spike.sh
@@ -5,4 +5,4 @@ set -eu
 SPIKE="${SPIKE:-spike}"
 
 # Set the ISA, cores/HARTs and RAM
-$SPIKE -p4 -m1024 -d --isa=rv64gcv_zba_zbb_zbc_zbs_zfh $1
+$SPIKE -p4 -m8192 -d --isa=rv64gcv_zba_zbb_zbc_zbs_zfh $1
